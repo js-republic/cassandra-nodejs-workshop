c3c7914d2f679bc9642b65305b3c9ec34bd21aed
Mathieu Breton <mathieu.breton@js-republic.com>
2018-02-08 17:16:54 +0100

    Change loadbalancing policie to be resilient to down

--- src/database/cassandra-client.database.js
+++ src/database/cassandra-client.database.js
@@ -1,5 +1,14 @@
-const { Client } = require('cassandra-driver');
+const {Client, policies} = require('cassandra-driver');
+
+const localDatacenter = 'datacenter1';
+const loadBalancingPolicy = new policies.loadBalancing.DCAwareRoundRobinPolicy(localDatacenter);
 
 module.exports = {
-  CassandraClient : new Client({ contactPoints: ['127.0.0.1:9042','127.0.0.1:9142'], keyspace : "workshop"})
+  CassandraClient: new Client({
+    contactPoints: ['127.0.0.1:9042', '127.0.0.1:9142'],
+    keyspace: 'workshop',
+    policies: {
+      loadBalancing: loadBalancingPolicy
+    }
+  })
 };

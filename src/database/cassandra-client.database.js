cd9686e1c4b08f1d775d215f69f31578f809f982
Mathieu Breton <mathieu.breton@js-republic.com>
2018-02-15 14:07:32 +0100

    Fix loadbalancer

--- src/database/cassandra-client.database.js
+++ src/database/cassandra-client.database.js
@@ -7,7 +7,7 @@ module.exports = {
     contactPoints: ['127.0.0.1:9042', '127.0.0.1:9142'],
     keyspace: 'workshop',
     policies: {
-      loadBalancing: new policies.loadBalancing.DCAwareRoundRobinPolicy(localDatacenter)
+      loadBalancing: new policies.loadBalancing.DCAwareRoundRobinPolicy(localDatacenter, 1)
     }
   })
 };
